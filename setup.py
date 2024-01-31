c2a55f4e7404bb1e266f39f7129a5c14dd905772
Antonin Blot <antonin.blot@crick.ac.uk>
2024-01-31 12:51:08 +0000

    [minor] update version number for dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flexilims",
-    version="v0.7",
+    version="v0.7-dev",
     url="https://github.com/znamlab/flexilims",
     license="MIT",
     author="Antonin Blot",
