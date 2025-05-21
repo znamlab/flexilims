222ef42b4ccd99dbc09c7899b3881c7e246ad0b5
Antonin Blot <antonin.blot@crick.ac.uk>
2025-05-21 17:26:32 +0100

    [feature] add cross_project_entity in get
    Default to False. It is never used in our projects and therefore not tested

--- tests/test_flexilims.py
+++ tests/test_flexilims.py
@@ -173,12 +173,13 @@ def test_get_req():
     # It works even without the project_id
     r = sess.get(datatype="dataset", name="test_dataset")
     assert (len(r) == 1) and (r[0]["name"] == "test_dataset")
+    r = sess.get(project_id=PROJECT_ID, name="test_dataset", datatype="dataset")
     # It works even without the project_id
     with pytest.raises(OSError) as exc_info:
         r = sess.get(datatype="dataset", name="test_dataset")
     assert (
         exc_info.value.args[0]
-        == "Error 400:  you need to provide project_id to query for dataset"
+        == "Error 400:  you need to provide project_id to query for [dataset]"
     )
 
 
